d35ddf2ad0a26868295b19de0b097a3ec9d3ef07
pancelor <pancelor@gmail.com>
2017-12-19 21:42:04 -0500

    add medallion gen stub

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod Items;
+mod Medallions;
 
 
 fn main() {
---
+++ src/medallions.rs
@@ -0,0 +1,15 @@
+pub enum MedallionGoal {
+  Bombos,
+  Ether,
+  Quake,
+}
+pub const ALL = {
+  MedallionGoal::Bombos,
+  MedallionGoal::Ether,
+  MedallionGoal::Quake,
+}
+
+pub fn setMedallions() {
+  // take in a world
+  unimplemented!();
+}
