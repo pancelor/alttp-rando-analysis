1e675d871743153549f21e12f48cea80a7895572
pancelor <pancelor@gmail.com>
2018-01-14 15:26:00 -0700

    map out POD zone connections

--- src/zones.rs
+++ src/zones.rs
@@ -17,6 +17,21 @@ pub enum Zone {
   POD7,
   POD8,
   POD9,
+  POD47,
+  POD29A,
+  POD29B,
 }
-
 pub use self::Zone::*;
+
+POD1   <=> POD2:   KeyPOD
+POD1   <=> POD8:   Bow
+POD8    => POD2:   Hammer
+POD2   <=> POD3:   KeyPOD
+POD2   <=> POD4:   KeyPOD
+POD4   <=> POD47:  KeyPOD
+POD47  <=> POD7:   Lamp
+POD4   <=> POD6:   Lamp
+POD4   <=> POD5:   KeyPOD
+POD2   <=> POD29A: Bow and Lamp and Hammer
+POD29A <=> POD29B: KeyPOD
+POD29B <=> POD9:   BigKeyPOD
