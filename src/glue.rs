a0f419a50f0b6f0022437078e822286108d18227
pancelor <pancelor@gmail.com>
2018-01-15 21:10:59 -0700

    ewwwww the mappings are gonna suck hard

--- src/glue.rs
+++ src/glue.rs
@@ -7,8 +7,10 @@
 use std::collections::{HashMap, HashSet};
 use super::{medallions, logic, world2, locations2, regions, items, zones, dungeons};
 use super::zones::{Zone, KeyDoor, ItemDoor};
+use super::zones::Zone::*;
 use super::dungeons::Dungeon;
 use super::locations2::Location2;
+use super::locations2::Location2::*;
 use super::items::Item;
 
 pub fn keyfrontier_from_dungeon(dungeon: Dungeon) -> HashSet<KeyDoor> {
@@ -32,11 +34,60 @@ pub fn keyfrontier_from_zone(zone: Zone) -> HashSet<KeyDoor> {
 }
 
 pub fn itemfrontier_from_zone(zone: Zone) -> Vec<ItemDoor> {
+  match zone {
+    // ah crap
+  }
   vec![] // TODO: impl
 }
 
 pub fn locations_from_zone(zone: Zone) -> HashSet<Location2> {
-  hashset!{} // TODO: impl
+  match zone {
+    Overworld => hashset!{
+      TempOverworld1,
+      TempOverworld2,
+      TempOverworld3,
+      TempOverworld4,
+      TempOverworld5,
+    },
+    POD1 => hashset!{
+      PalaceOfDarknessShooterRoom,
+    },
+    POD2 => hashset!{
+      PalaceOfDarknessStalfosBasement,
+      PalaceOfDarknessTheArenaBridge,
+    },
+    POD3 => hashset!{
+      PalaceOfDarknessBigKeyChest,
+    },
+    POD4 => hashset!{
+      PalaceOfDarknessCompassChest,
+    },
+    POD5 => hashset!{
+      PalaceOfDarknessHarmlessHellway,
+    },
+    POD6 => hashset!{
+      PalaceOfDarknessDarkBasementLeft,
+      PalaceOfDarknessDarkBasementRight,
+    },
+    POD7 => hashset!{
+      PalaceOfDarknessDarkMazeTop,
+      PalaceOfDarknessDarkMazeBottom,
+    },
+    POD8 => hashset!{
+      PalaceOfDarknessTheArenaLedge,
+      PalaceOfDarknessMapChest,
+    },
+    POD9 => hashset!{
+      PalaceOfDarknessHelmasaurKing,
+      PalaceOfDarknessPrize,
+    },
+    POD10 => hashset!{
+      PalaceOfDarknessBigChest,
+    },
+    POD47 => hashset!{},
+    POD29A => hashset!{},
+    POD29B => hashset!{},
+  }
 }
 
 
